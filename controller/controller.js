99fc57a8d6401bab922d15b0586fe5ed1f1074cc
KiritoKi <yaneliteboy@hotmail.com>
2022-10-09 21:06:01 -0300

    Updated

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,29 @@
+import user from '../model/user.js';
 import db from './db.js';
 
-function register() {
+function register(user) {
+    const params = [
+        user.getNome(),
+        user.getData_nascimento(),
+        user.getUsername(),
+        user.getPassword(),
+        user.Email()
+    ];
+    console.log("Inserting: " + params);
 
+    let sql = "INSERT INTO user ";
+    sql += "(nome, data_nascimento, username, password, email) VALUES (?,?,?,?,?);";
+    db.query(sql, params,
+        function (err) {
+            if (err) throw console.log("INSERT-ERROR FROM = " + params + "err = " + err)
+        }
+    );
 }
 
 function selectLogin(login_name) {
     return new Promise((resolve, reject) => {
         let sql = "SELECT * FROM users";
-        slq += "WHERE(login_name = ?)"
+        slq += "WHERE(username = ?)"
 
         const params = [login_name];
 
@@ -19,8 +35,8 @@ function selectLogin(login_name) {
                 console.log(result);
                 resolve(result[0]);
             }
-        )
-    })
+        );
+    });
 }
 
 function login() {
