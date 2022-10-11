f9e8246880bbf7903243961e39d4613ab34247b3
KiritoKi <yaneliteboy@hotmail.com>
2022-10-11 15:22:33 -0300

    ; 7

--- controller/db.js
+++ controller/db.js
@@ -2,8 +2,8 @@ import { createConnection } from 'mysql';
 
 var connection = createConnection({
     host: 'localhost',
-    user: 'kirito',
-    password: 'yanVP&123456',
+    user: 'USER',
+    password: 'PASSWORD',
     database: 'db_software_engineering'
 });
 
