c4cd6b8d8cff0a1e7218852eb7da819eec878e20
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-10-16 22:45:55 -0300

    update

--- controller/db.js
+++ controller/db.js
@@ -2,11 +2,11 @@ import { createConnection } from 'mysql';
 
 var connection = createConnection({
     host: 'localhost',
-    user: 'kirito',
-    password: 'yanVP&123456',
-    database: 'db_software_engineering'
+    user: 'fabio',
+    password: '123456789',
+    database: 'engenharia_software'
 });
 
 connection.connect();
 
-export default connection;"
+export default connection;
